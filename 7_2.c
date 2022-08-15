cb4214ddf5b1dff90ab1e6bfe61dcc47b149d365
Kyungtae Bae <Kyungtae@marvrus.com>
2022-08-15 14:58:00 +0900

    예제7-2

---
+++ 7_2.c
@@ -0,0 +1,18 @@
+//
+// Created by Xeon on 2022-08-15.
+//
+#include <stdio.h>
+
+int main(void) {
+    int i = 200;
+    int* ptr = &i;
+
+    *ptr = *ptr + 1;
+
+    printf("i = %d\n", i);
+    printf("*ptr = %d\n", *ptr);
+    printf("&i =%p\n", &i);
+    printf("ptr = %p\n", ptr);
+
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-
-int main() {
-    int i = 5;
-    int* p = &i;
-
-    printf("i = %d\n", i);
-    printf("&i = %p\n", &i);
-    printf("p = %p\n", p);
-    printf("&p = %p\n", &p);
-
-    return 0;
-}
+//#include <stdio.h>
+//
+//int main() {
+//    int i = 5;
+//    int* p = &i;
+//
+//    printf("i = %d\n", i);
+//    printf("&i = %p\n", &i);
+//    printf("p = %p\n", p);
+//    printf("&p = %p\n", &p);
+//
+//    return 0;
+//}
