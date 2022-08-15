28ac5452c0f4d3a53145b445691c972b3e3e8aa7
Kyungtae Bae <Kyungtae@marvrus.com>
2022-08-15 14:46:32 +0900

    예제7-1

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 
 int main() {
     int i = 5;
-    int *p = &i;
+    int* p = &i;
 
     printf("i = %d\n", i);
-    printf("&i = %d\n", &i);
+    printf("&i = %p\n", &i);
     printf("p = %p\n", p);
-    printf("&p = &p\n", &p);
+    printf("&p = %p\n", &p);
 
     return 0;
 }
